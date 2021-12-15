2200774df300fd2a655576aae974474019bfc7b0
gjf2a <ferrer@hendrix.edu>
2021-12-15 05:40:46 -0600

    Reorganized into multiple files.

---
+++ src/position.rs
@@ -0,0 +1,259 @@
+use std::{io, mem};
+use std::ops::{Add, AddAssign, Mul, MulAssign, Sub};
+use std::str::FromStr;
+use crate::make_io_error;
+use enum_iterator::IntoEnumIterator;
+
+#[derive(Debug,Copy,Clone,Eq,PartialEq,Ord,PartialOrd,Hash)]
+pub struct Position {
+    pub row: isize,
+    pub col: isize
+}
+
+impl Add for Position {
+    type Output = Position;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Position {col: self.col + rhs.col, row: self.row + rhs.row}
+    }
+}
+
+impl Sub for Position {
+    type Output = Position;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Position {col: self.col - rhs.col, row: self.row - rhs.row}
+    }
+}
+
+impl Mul<isize> for Position {
+    type Output = Position;
+
+    fn mul(self, rhs: isize) -> Self::Output {
+        Position {col: self.col * rhs, row: self.row * rhs}
+    }
+}
+
+impl AddAssign for Position {
+    fn add_assign(&mut self, rhs: Self) {
+        *self = *self + rhs;
+    }
+}
+
+impl MulAssign<isize> for Position {
+    fn mul_assign(&mut self, rhs: isize) {
+        *self = *self * rhs;
+    }
+}
+
+impl Position {
+    pub fn new() -> Self {
+        Position::from((0, 0))
+    }
+
+    pub fn from(pair: (isize,isize)) -> Self {
+        Position {col: pair.0, row: pair.1}
+    }
+
+    pub fn update(&mut self, d: Dir) {
+        let (nc, nr) = d.neighbor(self.col, self.row);
+        self.col = nc;
+        self.row = nr;
+    }
+
+    pub fn updated(&self, d: Dir) -> Self {
+        let (nc, nr) = d.neighbor(self.col, self.row);
+        Position {col: nc, row: nr}
+    }
+
+    pub fn next_in_grid(&self, width: usize, height: usize) -> Option<Position> {
+        let mut result = self.clone();
+        result.col += 1;
+        if result.col == width as isize {
+            result.col = 0;
+            result.row += 1;
+        }
+        if result.row < height as isize {Some(result)} else {None}
+    }
+
+    pub fn manhattan_distance(&self, other: Position) -> usize {
+        ((self.col - other.col).abs() + (self.row - other.row).abs()) as usize
+    }
+
+    pub fn is_manhattan_neighbor_of(&self, other: Position) -> bool {
+        self.manhattan_distance(other) == 1
+    }
+
+    pub fn manhattan_neighbors(&self) -> impl Iterator<Item=Position> + '_ {
+        ManhattanDir::into_enum_iter().map(|dir| *self + dir.position_offset())
+    }
+
+    pub fn neighbors(&self) -> impl Iterator<Item=Position> + '_ {
+        Dir::into_enum_iter().map(|dir| *self + dir.position_offset())
+    }
+}
+
+impl FromStr for Position {
+    type Err = io::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.len() >= 2 && s.contains(',') {
+            let mut chars = s.chars();
+            let first = chars.next().unwrap();
+            let last = chars.last().unwrap();
+            if first == '(' && last == ')' {
+                return Position::from_str(&s[1..s.len() - 1])
+            } else {
+                let parts: Vec<_> = s.split(',').collect();
+                if parts.len() == 2 {
+                    return match parts[0].trim().parse::<isize>()
+                        .and_then(|x| parts[1].trim().parse::<isize>().map(|y| (x, y))) {
+                        Ok(pair) => Ok(Position::from(pair)),
+                        Err(e) => make_io_error(e.to_string().as_str())
+                    }
+                }
+            }
+        }
+        make_io_error(format!("\"{}\" is not a Position.", s).as_str())
+    }
+}
+
+pub struct RowMajorPositionIterator {
+    width: usize, height: usize, next: Option<Position>
+}
+
+impl RowMajorPositionIterator {
+    pub fn new(width: usize, height: usize) -> Self {
+        RowMajorPositionIterator {width, height, next: Some(Position {col: 0, row: 0})}
+    }
+
+    pub fn in_bounds(&self) -> bool {
+        self.next.map_or(false, |n| n.col < self.width as isize && n.row < self.height as isize)
+    }
+}
+
+impl Iterator for RowMajorPositionIterator {
+    type Item = Position;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut future = self.next.and_then(|p| p.next_in_grid(self.width, self.height));
+        mem::swap(&mut future, &mut self.next);
+        future
+    }
+}
+
+pub fn indices_2d_vec<T>(width: usize, height: usize, func: fn(usize,usize)->T) -> Vec<Vec<T>> {
+    (0..height)
+        .map(|y| (0..width)
+            .map(|x| func(x, y))
+            .collect())
+        .collect()
+}
+
+pub trait DirType {
+    fn offset(&self) -> (isize,isize);
+
+    fn next(&self, p: Position) -> Position {
+        p + Position::from(self.offset())
+    }
+
+    fn position_offset(&self) -> Position {
+        Position::from(self.offset())
+    }
+
+    fn neighbor(&self, col: isize, row: isize) -> (isize,isize) {
+        let (d_col, d_row) = self.offset();
+        (col + d_col, row + d_row)
+    }
+
+    fn position_neighbor(&self, col: isize, row: isize) -> Position {
+        Position::from(self.neighbor(col, row))
+    }
+}
+
+#[derive(Debug,Clone,Copy,Eq,PartialEq,Ord,PartialOrd,IntoEnumIterator)]
+pub enum ManhattanDir {
+    N, E, S, W
+}
+
+impl DirType for ManhattanDir {
+    fn offset(&self) -> (isize, isize) {
+        match self {
+            ManhattanDir::N => (0, -1),
+            ManhattanDir::E => (1, 0),
+            ManhattanDir::S => (0, 1),
+            ManhattanDir::W => (-1, 0)
+        }
+    }
+}
+
+impl ManhattanDir {
+    pub fn inverse(&self) -> ManhattanDir {
+        match self {
+            ManhattanDir::N => ManhattanDir::S,
+            ManhattanDir::S => ManhattanDir::N,
+            ManhattanDir::E => ManhattanDir::W,
+            ManhattanDir::W => ManhattanDir::E
+        }
+    }
+
+    pub fn clockwise(&self) -> ManhattanDir {
+        match self {
+            ManhattanDir::N => ManhattanDir::E,
+            ManhattanDir::S => ManhattanDir::W,
+            ManhattanDir::E => ManhattanDir::S,
+            ManhattanDir::W => ManhattanDir::N
+        }
+    }
+}
+
+#[derive(Debug,Clone,Copy,Eq,PartialEq,Ord,PartialOrd,IntoEnumIterator)]
+pub enum Dir {
+    N, Ne, E, Se, S, Sw, W, Nw
+}
+
+impl DirType for Dir {
+    fn offset(&self) -> (isize,isize) {
+        match self {
+            Dir::N  => ( 0, -1),
+            Dir::Ne => ( 1, -1),
+            Dir::E  => ( 1,  0),
+            Dir::Se => ( 1,  1),
+            Dir::S  => ( 0,  1),
+            Dir::Sw => (-1,  1),
+            Dir::W  => (-1,  0),
+            Dir::Nw => (-1, -1)
+        }
+    }
+}
+
+impl Dir {
+    pub fn right(&self) -> Dir {
+        match self {
+            Dir::N => Dir::Ne,
+            Dir::Ne => Dir::E,
+            Dir::E => Dir::Se,
+            Dir::Se => Dir::S,
+            Dir::S => Dir::Sw,
+            Dir::Sw => Dir::W,
+            Dir::W => Dir::Nw,
+            Dir::Nw => Dir::N
+        }
+    }
+
+    pub fn rotated_degrees(&self, degrees: isize) -> Dir {
+        let mut steps = normalize_degrees(degrees) / 45;
+        let mut result = *self;
+        while steps > 0 {
+            steps -= 1;
+            result = result.right();
+        }
+        result
+    }
+}
+
+pub fn normalize_degrees(degrees: isize) -> isize {
+    let mut degrees = degrees;
+    while degrees < 0 {degrees += 360;}
+    degrees % 360
+}
---
+++ src/searchers.rs
@@ -0,0 +1,155 @@
+use std::collections::{BinaryHeap, BTreeMap, BTreeSet, VecDeque};
+use std::fmt::Debug;
+use common_macros::b_tree_set;
+use trait_set::trait_set;
+use derive_getters::Getters;
+
+trait_set! {
+    pub trait SearchNode = Ord + Eq + Clone + Debug;
+}
+
+pub trait SearchQueue<T> {
+    fn new() -> Self;
+    fn enqueue(&mut self, item: &T);
+    fn dequeue(&mut self) -> Option<T>;
+    fn len(&self) -> usize;
+}
+
+impl <T:Clone+Debug> SearchQueue<T> for VecDeque<T> {
+    fn new() -> Self {VecDeque::new()}
+    fn enqueue(&mut self, item: &T) {self.push_back(item.clone());}
+    fn dequeue(&mut self) -> Option<T> {self.pop_front()}
+    fn len(&self) -> usize {self.len()}
+}
+
+impl <T:Clone+Debug> SearchQueue<T> for Vec<T> {
+    fn new() -> Self {Vec::new()}
+    fn enqueue(&mut self, item: &T) {self.push(item.clone());}
+    fn dequeue(&mut self) -> Option<T> {self.pop()}
+    fn len(&self) -> usize {self.len()}
+}
+
+impl <T:SearchNode> SearchQueue<T> for BinaryHeap<T> {
+    fn new() -> Self {BinaryHeap::new()}
+    fn enqueue(&mut self, item: &T) {self.push(item.clone());}
+    fn dequeue(&mut self) -> Option<T> {self.pop()}
+    fn len(&self) -> usize {self.len()}
+}
+
+#[derive(Debug, Clone)]
+pub struct ParentMapQueue<T: SearchNode, Q: SearchQueue<T>> {
+    queue: Q,
+    last_dequeued: Option<T>,
+    pub(crate) parent_map: BTreeMap<T, Option<T>>
+}
+
+impl <T: SearchNode, Q: SearchQueue<T>> ParentMapQueue<T, Q> {
+    pub fn parent_of(&self, item: &T) -> &Option<T> {
+        self.parent_map.get(item).unwrap_or(&None)
+    }
+
+    pub fn path_back_from(&self, end: &T) -> VecDeque<T> {
+        path_back_from(end, &self.parent_map)
+    }
+}
+
+impl <T: SearchNode, Q: SearchQueue<T>> SearchQueue<T> for ParentMapQueue<T, Q> {
+    fn new() -> Self {
+        ParentMapQueue {parent_map: BTreeMap::new(), queue: Q::new(), last_dequeued: None}
+    }
+
+    fn enqueue(&mut self, item: &T) {
+        if !self.parent_map.contains_key(item) {
+            self.parent_map.insert(item.clone(), self.last_dequeued.clone());
+            self.queue.enqueue(item);
+        }
+    }
+
+    fn dequeue(&mut self) -> Option<T> {
+        let dequeued = self.queue.dequeue();
+        self.last_dequeued = dequeued.clone();
+        dequeued
+    }
+
+    fn len(&self) -> usize {self.queue.len()}
+}
+
+#[derive(Clone, Debug, Getters)]
+pub struct SearchResult<Q> {
+    enqueued: usize,
+    dequeued: usize,
+    pub(crate) open_list: Q
+}
+
+pub fn search<T, S, Q>(mut open_list: Q, mut add_successors: S) -> SearchResult<Q>
+    where T: Clone, Q: SearchQueue<T>, S: FnMut(&T, &mut Q) {
+    let mut enqueued = open_list.len();
+    let mut dequeued = 0;
+    loop {
+        match open_list.dequeue() {
+            Some(candidate) => {
+                dequeued += 1;
+                let before = open_list.len();
+                add_successors(&candidate, &mut open_list);
+                assert!(open_list.len() >= before);
+                enqueued += open_list.len() - before;
+            }
+            None => return SearchResult {enqueued, dequeued, open_list}
+        }
+    }
+}
+
+pub fn breadth_first_search<T,S>(start_value: &T, add_successors: S) -> BTreeMap<T,Option<T>>
+    where T: SearchNode, S: FnMut(&T, &mut ParentMapQueue<T, VecDeque<T>>) {
+    let mut open_list = ParentMapQueue::new();
+    open_list.enqueue(start_value);
+    search(open_list, add_successors).open_list.parent_map
+}
+
+pub fn path_back_from<T: SearchNode>(end: &T, parent_map: &BTreeMap<T,Option<T>>) -> VecDeque<T> {
+    let mut path = VecDeque::new();
+    let mut current = end;
+    loop {
+        path.push_front(current.clone());
+        match parent_map.get(&current).unwrap() {
+            None => break,
+            Some(parent) => {current = parent;}
+        }
+    }
+    path
+}
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct AdjacencySets {
+    graph: BTreeMap<String,BTreeSet<String>>
+}
+
+impl AdjacencySets {
+    pub fn new() -> Self {
+        AdjacencySets {graph: BTreeMap::new()}
+    }
+
+    pub fn keys(&self) -> impl Iterator<Item=&str> {
+        self.graph.keys().map(|s| s.as_str())
+    }
+
+    pub fn neighbors_of(&self, node: &str) -> Option<&BTreeSet<String>> {
+        self.graph.get(node)
+    }
+
+    pub fn connect2(&mut self, start: &str, end: &str) {
+        self.connect(start, end);
+        self.connect(end, start);
+    }
+
+    pub fn connect(&mut self, start: &str, end: &str) {
+        match self.graph.get_mut(start) {
+            None => {
+                self.graph.insert(start.to_string(), b_tree_set! {end.to_string()});
+            }
+            Some(connections) => {
+                connections.insert(end.to_string());
+            }
+        }
+    }
+}
