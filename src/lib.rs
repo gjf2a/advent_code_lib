2389dfb0fbd10623e2d52f3ec09d3065a6a18f5a
gjf2a <ferrer@hendrix.edu>
2023-12-21 12:38:57 -0600

    Allow extra arguments with chooser_main()

--- src/lib.rs
+++ src/lib.rs
@@ -54,13 +54,13 @@ impl FromStr for Part {
     }
 }
 
-pub fn chooser_main(code: fn(&str, Part) -> anyhow::Result<()>) -> anyhow::Result<()> {
+pub fn chooser_main(code: fn(&str, Part, &[String]) -> anyhow::Result<()>) -> anyhow::Result<()> {
     let args: Vec<String> = env::args().collect();
     if args.len() < 3 {
-        println!("Usage: {} filename (one|two)", args[0]);
+        println!("Usage: {} filename (one|two) [options]", args[0]);
         Ok(())
     } else {
-        code(args[1].as_str(), args[2].parse().unwrap())
+        code(args[1].as_str(), args[2].parse().unwrap(), &args[3..])
     }
 }
 
